5e98b06ce4c61cc3c1bfb98f27968612695b74a8
ritesh123-long <riteshpadme6@gmail.com>
2025-10-20 13:29:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 token = "8071518517:AAF3huWVr6uAUXtEN3eW6zvs24C07SnZiKk"  # telegram token
 logs = None  # logs channel id, if none set to None
-max_filesize = 1000000000  # bytes
+max_filesize = 10000000000  # bytes
 output_folder = "outputs"  # folder where videos will be downloaded temporarily
