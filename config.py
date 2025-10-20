d25d1f6274c89eebce4e6eacd2cd9d9f6f362022
ritesh123-long <riteshpadme6@gmail.com>
2025-10-20 14:32:00 +0530

    Update max_filesize to 2 GB in config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 token = "8071518517:AAF3huWVr6uAUXtEN3eW6zvs24C07SnZiKk"  # telegram token
 logs = None  # logs channel id, if none set to None
-max_filesize = 2 * 1024 * 1024 * 1024  # 2 GB
+max_filesize = 2000000000  # 2 GB
 # bytes
 output_folder = "output"  # folder where videos will be downloaded temporarily
